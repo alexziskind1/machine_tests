c8f36274ada6958c3a54368e1cd28a549b89c2a1
Alex Ziskind <alex@nuvious.com>
2024-07-01 10:15:48 -0400

    clean data - reordered models

--- battery/analysis/clean_data.py
+++ battery/analysis/clean_data.py
@@ -3,27 +3,29 @@ import pandas as pd
 
 # Updated dictionary to include tuples (model_name, battery_size Wh)
 filename_to_model = {
-    "m1pro14": ("MacBook Pro 14 M1", "69.6"),
-    "m2max16": ("MacBook Pro 16 M2 Max", "99.6"),
-    "corei9_mbp2019": ("MacBook Pro 19 Core i9", "100"),
-    "xelite_galaxybook4edge16": ("Galaxy Book 4 16 Edge X Elite", "61.8"),
-    "coreU7_155H_xps13": ("XPS 13 Core Ultra 7 155H", "55"),
-    "xelite_vivobooks15": ("Asus VivoBook S15 X Elite", "70"),
-    "m2mba13": ("MacBook Air 13 M2", "52.6"),
+    # MacBook comparisons Jan 2024
     "corei5_2017mba": ("MacBook Air 2017 Core i5", "54"),
-    "xelite_surface_laptop7_13": ("Surface Laptop 7 13 X Elite", "54"),
-    "m2max16_2": ("MacBook Pro 16 M2 Max (2)", "99.6"),
+    "corei9_mbp2019": ("MacBook Pro 19 Core i9", "100"),
     "m1mba": ("MacBook Air M1", "49.9"),
+    "m1pro14": ("MacBook Pro 14 M1", "69.6"),
+    "m2mba13": ("MacBook Air 13 M2", "52.6"),
+    "m2max16": ("MacBook Pro 16 M2 Max", "99.6"),
     "m3mbp": ("MacBook Pro M3", "69.6"),
-    "xplus_surface_laptop7_13": ("Surface Laptop 7 13 X Plus", "54"),
-    "xelite_xps13": ("XPS 13 X Elite", "55"),
-    "m3mba": ("MacBook Air 13 M3", "52.6"),
     "m3pro14": ("MacBook Pro 14 M3 Pro", "69.6"),
     "m3max16": ("MacBook Pro 16 M3 Max", "99.6"),
+    # New models added June 2024
+    "m2max16_2": ("MacBook Pro 16 M2 Max (2)", "99.6"),
+    "m3mba": ("MacBook Air 13 M3", "52.6"),
     "coreU7_165H_surface_laptop6": (
         "Surface Laptop 6 Core Ultra 7 165H",
         "47",
     ),
+    "xelite_surface_laptop7_13": ("Surface Laptop 7 13 X Elite", "54"),
+    "xplus_surface_laptop7_13": ("Surface Laptop 7 13 X Plus", "54"),
+    "coreU7_155H_xps13": ("XPS 13 Core Ultra 7 155H", "55"),
+    "xelite_xps13": ("XPS 13 X Elite", "55"),
+    "xelite_galaxybook4edge16": ("Galaxy Book 4 16 Edge X Elite", "61.8"),
+    "xelite_vivobooks15": ("Asus VivoBook S15 X Elite", "70"),
 }
 
 
